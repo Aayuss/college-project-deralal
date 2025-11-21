2ef58296522c72d99bf67b477301cfb665e3adcb
Ayus <ayus222poudel@gmail.com>
2025-11-21 08:57:23 +0545

    logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,8 @@ export const metadata: Metadata = {
   description: "Created for our landlords and rentees to connect seamlessly.",
   generator: "aayush",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/logo.png",
+    apple: "/logo.png",
   },
 }
 
